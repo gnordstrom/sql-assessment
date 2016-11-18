f37fbf24b9a8bb70625344236f07a31293644ead
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-11-18 13:59:40 -0700

    Copied assessment files into new repo

---
+++ db/vehicle_create_seed.sql
@@ -0,0 +1,10 @@
+-- It may be helpful to drop and reinstantilize the table when doing
+-- the tests in case you delete users/cars the tests are expecting to see
+-- DROP TABLE IF EXISTS vehicles;
+
+('Toyota', 'Camry', 1991, 1),
+('Honda', 'Civic', 1995, 1),
+('Ford', 'Focus', 2005, 1),
+('Ford', 'Taurus', 2003, 2),
+('VW', 'Bug', 2010, 2),
+('Mini', 'Coup', 2013, 3);
---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var massive = require('massive');
+//Need to enter username and password for your database
+var connString = "postgres://username:password@localhost/assessbox";
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//The test doesn't like the Sync version of connecting,
+//  Here is a skeleton of the Async, in the callback is also
+//  a good place to call your database seeds.
+var db = massive.connect({connectionString : connString},
+  function(err, localdb){
+    db = localdb;
+    // app.set('db', db);
+    //
+    // db.user_create_seed(function(){
+    //   console.log("User Table Init");
+    // });
+    // db.vehicle_create_seed(function(){
+    //   console.log("Vehicle Table Init")
+    // });
+})
+
+app.listen('3000', function(){
+  console.log("Successfully listening on : 3000")
+})
+
+module.exports = app;
